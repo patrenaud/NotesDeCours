58f3ae3d01ed3cfab06f78a2e5fd9d0bc676a4f3
patrenaud <pat_renaud@hotmail.com>
2018-06-23 10:31:16 -0400

    Inventaire

---
+++ UI et Inventaire/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    	
+	private void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.I))
+        {
+            InventoryManager.Instance.CallInventory();
+        }
+	}
+}
