b8975ecc42f994b5a85300607a4261e9f5a660c2
Patrick R <1810057@bart.ca>
2018-07-09 11:00:32 -0400

    EventManager and stuff

---
+++ Actions/Assets/Scripts/MAINUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MAINUI : MonoBehaviour
+{
+    public Slider m_LifeBar;
+
+    private void Start()
+    {
+        // GAMEMANAGER.Instance.m_Player.m_HurtAction += SetLifeValue;
+
+        EventManager.Instance.RegisterEvent(EventID.UpdateHP, SetLifeValue);
+        //Debug.Log("Current HP:" + m_LifeBar.value);
+
+    }
+
+    private void SetLifeValue(object a_value)
+    {
+        m_LifeBar.value = (int)a_value;
+    }
+
+    private void OnDestroy()
+    {
+
+        // GAMEMANAGER.Instance.m_Player.m_HurtAction -= SetLifeValue;
+
+        EventManager.Instance.UnregisterEvent(EventID.UpdateHP, SetLifeValue);
+        //Debug.Log("Current HP:" + m_LifeBar.value);
+
+    }
+}
---
+++ EventManager/Assets/Scripts/EventManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour 
+{
+	// la variable se track moins bien en utilisant cette méthode pour le singleton
+	public static EventManager Instance { get; private set ;}
+
+	private Dictionary<EventID, Action<object>> m_EventDict;
+
+	private void Awake()
+	{
+		if(Instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = this;
+		}
+		DontDestroyOnLoad(this);
+		m_EventDict = new Dictionary<EventID, Action<object>>();
+	}
+
+	public void RegisterEvent(EventID i_ID, Action <object> i_CallBack)
+	{
+		if(m_EventDict.ContainsKey(i_ID))
+		{
+			m_EventDict[i_ID] += i_CallBack;
+		}
+		else
+		{
+			m_EventDict.Add(i_ID, i_CallBack);
+		}
+	}
+
+	public void UnregisterEvent(EventID i_ID, Action<object> i_CallBack)
+	{
+		if(m_EventDict.ContainsKey(i_ID))
+		{
+			m_EventDict[i_ID] -= i_CallBack;
+		}
+		else
+		{
+			Debug.LogError("The event you are unregistering doesn't exist.");
+		}
+	}
+
+	public void DispatchEvent(EventID i_ID, object i_Param = null)
+	{
+		if(m_EventDict.ContainsKey(i_ID))
+		{
+			m_EventDict[i_ID](i_Param);
+		}
+		else
+		{
+			Debug.LogError("The event you are dispatching doesn't exist.");
+		}
+	}
+}
